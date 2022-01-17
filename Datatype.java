62c5ea073ebce50d50dbbd977536ba683710321a
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2022-01-17 18:47:08 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,12 @@
+class Datatype 
+{
+  public static void main( String [] args)
+  {
+      int age = 56;
+      String name = "Azami";
+      float weight = 5.89f;
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(weight);
+  }
+}
