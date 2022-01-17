704f9372b32086efee3ad4045adac81c297b0919
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2022-01-17 18:46:45 +0530

    Add files via upload
    Comprises of code of swapping two numbers using temp variable.

---
+++ swap1.java
@@ -0,0 +1,15 @@
+class swap1
+{
+    public static void main(String [] args)
+    {
+        int a=10;
+        int b=56;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
