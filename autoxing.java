63baef8962296272213bae9b03ce0967ab623b5a
A-s-t-h-a <79190491+A-s-t-h-a@users.noreply.github.com>
2022-01-18 14:32:22 +0530

    Add files via upload
    GFG example for Autoboxing which is a primitive type of variable turning into a wrapper class object

---
+++ autoxing.java
@@ -0,0 +1,11 @@
+class autoxing
+{
+    public static void main (String[] args)
+    {
+        Integer x1=400,x2=400;
+        if(x1==x2)
+        System.out.println("same");
+        else
+        System.out.println("not same");
+    }
+}
